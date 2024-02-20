d8731d8c987682127c21dae5bcd2d2f333b70112
AishSI <aishsi@gmail.com>
2024-02-20 17:16:03 +0700

    feat: add optimal solution 'removeProperties' task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -64,8 +64,13 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const currObj = obj;
+  keys.forEach((key) => {
+    delete currObj[key];
+  });
+
+  return currObj;
 }
 
 /**
