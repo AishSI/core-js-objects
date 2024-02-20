43a18a28596d2cb6d8909ac8d7f8b5c527993a36
AishSI <aishsi@gmail.com>
2024-02-20 17:58:06 +0700

    feat: add solution 'makeWord' task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -136,8 +136,18 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const res = [];
+  const arrLetters = Object.entries(lettersObject);
+
+  arrLetters.forEach((item) => {
+    const [letter, positions] = item;
+    positions.forEach((pos) => {
+      res[pos] = letter;
+    });
+  });
+
+  return res.join('');
 }
 
 /**
